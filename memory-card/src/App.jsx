10b3454b7ee5e1d95c3ce5e199ae1000893c0685
Stephan Allek Weigert <117909784+stpafk@users.noreply.github.com>
2023-08-01 17:10:16 +0000

    started components

--- memory-card/src/App.jsx
+++ memory-card/src/App.jsx
@@ -1,7 +1,22 @@
 import { useState } from 'react'
 import './App.css'
+import Header from './components/Header'
+import CardHandler from './components/Card'
 
 function App() {
+
+    return(
+        <>
+        <div className="mc header">
+            <h1 className='mc title'>Memory Cat</h1>
+            <p className='mc subtitle'>A memory card based on cats!</p>
+            <Header />
+        </div>
+        <div className="mc main">
+            <CardHandler />
+        </div>
+        </>
+    )
 
 }
 
---
+++ memory-card/src/components/Header.jsx
@@ -0,0 +1,8 @@
+import { useState } from 'react'
+import './App.css'
+
+function Header() {
+
+}
+
+export default Header;
