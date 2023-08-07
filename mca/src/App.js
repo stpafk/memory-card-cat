217ebe8db1dc9bbb80cf8d17dfce78e6d48322e0
Stephan Allek Weigert <117909784+stpafk@users.noreply.github.com>
2023-08-07 18:04:21 +0000

    started state and handleClick

--- mca/src/App.js
+++ mca/src/App.js
@@ -4,15 +4,30 @@ import CardHandler from './components/CardHandler'
 
 export default function App() {
 
+    const [score, setScore] = useState(0);
+    const [cards, setCards] = useState(Array(10).fill(false));
+
+    const handleClick = (cardIdx) => {
+        if (!cards[cardIdx]) {
+            const newCards = [...cards];
+            newCards[cardIdx] = true;
+            setCards(newCards);
+            setScore(score + 1);
+        }
+
+        setScore(0);
+    }
+
+
     return(
         <>
         <div className="mc header">
             <h1 className='mc title'>Memory Cat</h1>
             <p className='mc subtitle'>A memory card based on cats!</p>
-            <Header />
+            <Header score={score}/>
         </div>
         <div className="mc main">
-            <CardHandler />
+            <CardHandler handleClick={handleClick}/>
         </div>
         </>
     )
