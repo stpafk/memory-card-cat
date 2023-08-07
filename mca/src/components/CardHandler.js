fdd880d1694f0d63be66a88eade3ae8146761eca
Stephan Allek Weigert <117909784+stpafk@users.noreply.github.com>
2023-08-07 19:41:52 +0000

    added cat info

--- mca/src/components/CardHandler.js
+++
@@ -1,9 +0,0 @@
-//import { useState } from 'react'
-
-function CardHandler() {
-    
-
-
-}
-
-export default CardHandler;
--- mca/src/views/Cats.js
+++ mca/src/views/Cats.js
@@ -1,8 +1,62 @@
 
-const CAT = {
+const CAT = [
+    {
+        id: 0,
+        src: "./-siam.jpeg",
+        name: "Siamese",
+        clicked: false,
+    },
 
-}
+    {
+        id: 1,
+        src: "./abob.jpg",
+        name: "American Bobtail",
+        clicked: false,
+    },
+
+    {
+        id: 2,
+        src: "./abys.jpg",
+        name: "Abyssinian",
+        clicked: false,
+    },
+
+    {
+        id: 3,
+        src: "./acur.jpg",
+        name: "American Curl",
+        clicked: false,
+    },
+
+    {
+        id: 4,
+        src: "./amau_l.jpg",
+        name: "Arabian Mau",
+        clicked: false,
+    },
+
+    {
+        id: 5,
+        src: "./bamb.jpg",
+        name: "Bambino",
+        clicked: false,
+    },
+
+    {
+        id: 6,
+        src: "./bomb.jpg",
+        name: "Bombay",
+        clicked: false,
+    },
+
+    {
+        id: 7,
+        src: "./siberian.jpg",
+        name: "Siberian Cat",
+        clicked: false,
+    },
+
+]
 
 export default CAT;
 
-//live_xBAB5e1tson3bbpcNdXLPM9r0v3gFxn1dkXqlrr1kcdPgZcqbfC7YZpWMU9ABPDX
