4b7be83d3bafc2fb2e19712af7353cea32f2bb3c
Stephan Allek Weigert <117909784+stpafk@users.noreply.github.com>
2023-08-18 18:26:19 +0000

    fix cat img not loading

--- mca/src/views/Cats.js
+++ mca/src/views/Cats.js
@@ -2,56 +2,56 @@
 const Cats = [
     {
         id: 0,
-        src: "../assets/-siam.jpeg",
+        src: "./assets/-siam.jpeg",
         name: "Siamese",
         clicked: false,
     },
 
     {
         id: 1,
-        src: "../assets/abob.jpg",
+        src: "./assets/abob.jpg",
         name: "American Bobtail",
         clicked: false,
     },
 
     {
         id: 2,
-        src: "../assets/abys.jpg",
+        src: "./assets/abys.jpg",
         name: "Abyssinian",
         clicked: false,
     },
 
     {
         id: 3,
-        src: "../assets/acur.jpg",
+        src: "./assets/acur.jpg",
         name: "American Curl",
         clicked: false,
     },
 
     {
         id: 4,
-        src: "../assets/amau_l.jpg",
+        src: "./assets/amau_l.jpg",
         name: "Arabian Mau",
         clicked: false,
     },
 
     {
         id: 5,
-        src: "../assets/bamb.jpg",
+        src: "./assets/bamb.jpg",
         name: "Bambino",
         clicked: false,
     },
 
     {
         id: 6,
-        src: "../assets/bomb.jpg",
+        src: "./assets/bomb.jpg",
         name: "Bombay",
         clicked: false,
     },
 
     {
         id: 7,
-        src: "../assets/siberian.jpg",
+        src: "./assets/siberian.jpg",
         name: "Siberian Cat",
         clicked: false,
     },
